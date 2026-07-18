d89966d4cad0752b3a055a426fdd7864bdbc453a
DZDEVreal <dz.dev.horror@gmail.com>
2026-07-18 19:01:20 +0100

    more work i guess

--- module/data.py
+++ module/data.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import decode_image
+
+class Custom_DataSet(Dataset):
+    def __init__(self):
+
+    def __len__(self):
+
+    def __getitem__(self):
+    
