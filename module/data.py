8c82613d5f6d2e1674b0ac1be91f2584b3143a92
DZDEVreal <dz.dev.horror@gmail.com>
2026-07-18 19:43:57 +0100

    data thing idk

--- module/data.py
+++ module/data.py
@@ -1,13 +1,25 @@
 import os
 import pandas as pd
 from torch.utils.data import Dataset
-from torchvision.transforms import v2
 from torchvision.io import decode_image
 
+
 class Custom_DataSet(Dataset):
-    def __init__(self):
+    def __init__(self, file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
 
     def __len__(self):
+        return len(self.img_labels)
 
-    def __getitem__(self):
-    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = decode_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
