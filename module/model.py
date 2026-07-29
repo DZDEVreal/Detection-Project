039d3d436a3270e583c361fcf647ea475cb708f3
DZDEVreal <dz.dev.horror@gmail.com>
2026-07-29 17:59:59 +0100

    Back to Swin-T with higher dropout, keep new reg pipeline

--- module/model.py
+++ module/model.py
@@ -4,28 +4,30 @@ import torchvision.models as models
 class VisionModel(nn.Module):
     def __init__(self, output_shape: int, freeze_base: bool = False):
         super().__init__()
-        self.base = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
+        self.base = models.swin_t(weights=models.Swin_T_Weights.DEFAULT)
 
         if freeze_base:
             for param in self.base.parameters():
                 param.requires_grad = False
 
-        nfc = self.base.classifier[1].in_features
+        nfc = self.base.head.in_features
 
         if freeze_base:
-            self.base.classifier = nn.Sequential(
-                nn.Dropout(0.5),
+            self.base.head = nn.Sequential(
                 nn.Linear(nfc, 256),
                 nn.ReLU(),
-                nn.Dropout(0.4),
+                nn.Dropout(0.5),
                 nn.Linear(256, output_shape)
             )
         else:
-            for i in range(6):
-                for param in self.base.features[i].parameters():
-                    param.requires_grad = False
-            self.base.classifier = nn.Sequential(
-                nn.Dropout(0.4),
+            for param in self.base.features[0].parameters():
+                param.requires_grad = False
+            for param in self.base.features[1].parameters():
+                param.requires_grad = False
+            for param in self.base.features[2].parameters():
+                param.requires_grad = False
+            self.base.head = nn.Sequential(
+                nn.Dropout(0.35),
                 nn.Linear(nfc, output_shape)
             )
 
